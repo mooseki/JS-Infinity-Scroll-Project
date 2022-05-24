9507a7a1b802d39897589bbd7906c111ed38e231
Antony Ekirapa <85803523+NueMe@users.noreply.github.com>
2022-05-24 08:59:16 +0300

    added missing apiKey on line 5

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // Unsplash API
 
 const count = 10;
-const apiKey = 's4BM0MhEJQ7h2_lfFKEK4PAUbfcBCl6L1kGc310Sv_c';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${}&count=${count}`;
+const apiKey = "qvB2sUcByvh5AXUxAZFlBpn635GrbYVvPxo663s6zjM";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Get photos from Unspash API
 async function getPhotos() {
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data)
-    } catch (error) {
-        // Catch Error Here
-    }
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    // Catch Error Here
+  }
 }
 
 // On Load
